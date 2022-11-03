22ee577098f85aa99bdb8cd5cc00b49d57b5e70a
Ion <mindru.ion97@gmail.com>
2022-11-03 11:13:58 +0200

    fix

--- second_taks.py
+++ second_taks.py
@@ -5,7 +5,6 @@ from sqlmodel import SQLModel, create_engine, Session, select, Field
 import schedule
 import time
 
-
 sqlite_url = f"postgresql://point:point@db:5438/point"
 engine = create_engine(sqlite_url, echo=True)
 session = Session(engine)
@@ -19,10 +18,21 @@ def send_message():
          """)
     result = results.first()
     total = result.sum.total_seconds() / 60 / 60 if result.sum else 0
+    results = session.exec("""
+        select Sum(track.ended_at - track.started_at)
+        from track
+        where Date(started_at) BETWEEN Date(now())- interval '1 day' and Date(now());
+    """)
+    result = results.first()
+    yesterday = result.sum.total_seconds() / 60 / 60 if result.sum else 0
     import asyncio
     loop = asyncio.get_event_loop()
+    if yesterday:
+        message = f"Krasava brat!!!!! ne zdavaisea, o sa se primeasca, ieri ai lucrat {yesterday:2.f} mai numai {100 - total:.2f} pina la scopul tau"
+    else:
+        message = f"Brat ce za huinea??!!!!! ne zdavaisea, o sa se primeasca, ieri ai lucrat {yesterday:2.f} mai tuhma {100 - total:.2f} pina la scopul tau"
     coroutine = bot.send_message(744570127,
-                                 f"Davai brat!!!!! ne zdavaisea, o sa se primeasca, mai inca {100 - total:.2f} pina la scopul tau")
+                                 message)
     loop.run_until_complete(coroutine)
 
 
